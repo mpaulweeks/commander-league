6fd5821b850581a4cf4bde1e5e8c9dad6db57340
mpaulweeks <mpaulweeks@gmail.com>
2015-12-17 19:03:09 -0500

    repo progress

--- rb/repo.rb
+++ rb/repo.rb
@@ -4,31 +4,43 @@ require_relative 'store'
 STATUS_IN_MAINDECK = 'maindeck'
 STATUS_IN_SIDEBOARD = 'sideboard'
 
-def Repository()
+class Repository
   DB_CACHE = {}
+
+  def initialize()
+    refresh_cache()
+  end
 
   def refresh_cache()
     DB_CACHE = load_database()
   end
 
-  refresh_cache()
-
   def load_binder(user_slug)
     out_status = {}
     DB_CACHE[KEY_STATUS][user_slug].each do |cs|
       card_name = cs["card_name"]
-      is_current = true
+      is_latest = true # && cs["timestamp"] < timestamp
       if out_status.has_key?(cs["status"])
-        is_current = out_status[card_name]["timestamp"]] < cs["timestamp"]
+        is_latest = out_status[card_name]["timestamp"]] < cs["timestamp"]
       end
-      if is_current
+      if is_latest
         out_status[card_name] = cs
       end
     end
 
-    out_cards = {"maindeck" => [], "sideboard" => []}
-    # process out_status
+    out_cards = {STATUS_IN_MAINDECK => [], STATUS_IN_SIDEBOARD => []}
+    out_status.each do |card_name, card_status|
+      card = DB_CACHE[KEY_CARD][card_name]
+      out_cards[card_status][card_name] = card
+    end
     
     out_hash = {"user_id" => user_slug, "cards" => out_cards}
     return out_hash
   end
+
+  def save_binder(view_binder)
+    user_slug = view_binder['user_slug']
+    old_binder = load_binder(user_slug)
+    new_binder = view_binder['cards']
+  end
+end
