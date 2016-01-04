e11a9e339e8cae0bb2f82354e8bc07f15470a291
mpaulweeks <mpaulweeks@gmail.com>
2016-01-03 23:31:04 -0500

    make test more specific, improve naming

--- rb/test/update_prices.rb
+++ rb/test/update_prices.rb
@@ -11,29 +11,31 @@ class UpdatePricesTest < Minitest::Test
     FileUtils.cp('json/test/database.json', @test_path)
   end
 
-  def assert_changes(card_name, before_price, after_price)
+  def assert_changes(card_name, market_price, expected_before, expected_after)
     FilePath.stub :database, @test_path do
-      Market.stub :get_price, after_price do
+      Market.stub :get_price, market_price do
         before_db = Store.load_database
-        assert_equal before_price, before_db[Store::CARD][card_name]['price']
-        update_prices()
+        assert_equal expected_before, before_db[Store::CARD][card_name]['price']
+        update_prices
         after_db = Store.load_database
-        assert_equal after_price, after_db[Store::CARD][card_name]['price']
+        assert_equal expected_after, after_db[Store::CARD][card_name]['price']
       end
     end
   end
 
   def test_updates_prices
     card_name = 'Abandon Hope'
-    before_price = 0.21
-    after_price = 12.34
-    assert_changes card_name, before_price, after_price
+    market_price = 12.34
+    expected_before = 0.21
+    expected_after = market_price
+    assert_changes card_name, market_price, expected_before, expected_after
   end
 
   def test_forgets_prices
     card_name = 'Abrupt Decay'
-    before_price = 13.25
-    after_price = nil
-    assert_changes card_name, before_price, after_price
+    market_price = 14.25
+    expected_before = 13.25
+    expected_after = nil
+    assert_changes card_name, market_price, expected_before, expected_after
   end
 end
