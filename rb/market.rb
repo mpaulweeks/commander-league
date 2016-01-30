48149679f47c8304e115d815254435077c5b6a5b
mpaulweeks <mpaulweeks@gmail.com>
2016-01-29 22:18:55 -0500

    update market and tests for new format

--- rb/market.rb
+++ rb/market.rb
@@ -18,17 +18,29 @@ class Market
 
   def self.parse_combodeck_json(card_name, json_str)
     json_dict = JSON.parse json_str
+    min_price = nil
     json_dict['Cards'].each do |card|
       if card['CardName'] == card_name
-        return card['PriceInCents']
+        card['Printings'].each do |printing|
+          printing_price = printing['PriceCentsPaper']
+          if printing_price && printing_price > 0
+            if min_price.nil? || printing_price < min_price
+              min_price = printing_price
+            end
+          end
+        end
       end
     end
+    return min_price
   end
 
   def self.get_price(card_name)
     url = COMBODECK_URL % card_name
     json_str = self.fetch_url(url)
     price = self.parse_combodeck_json(card_name, json_str)
+    if price.nil?
+      raise 'No legal price found'
+    end
     return price
   end
 end
