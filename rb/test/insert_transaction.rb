c9fc2616916664b3f84c93f2940fe7d543bb7b3f
mpaulweeks <mpaulweeks@gmail.com>
2017-01-14 15:38:12 -0500

    fix tests

--- rb/test/insert_transaction.rb
+++ rb/test/insert_transaction.rb
@@ -8,15 +8,14 @@ class InsertTransactionTest < Minitest::Test
 
   def setup
     @test_users = 'rb/test/tmp/users.json'
-    @repo = Repo.new
     FileUtils.cp(FilePath.test_users, @test_users)
   end
 
-  def get_balance
+  def get_balance(repo)
     balance = {}
     db_cache = Store.load_database
     db_cache[Store::USER].each do |user_slug, user|
-      user_info = @repo.load_user_info(db_cache, user_slug)
+      user_info = repo.load_user_info(db_cache, user_slug)
       balance[user_slug] = user_info[:balance]
     end
     return balance
@@ -25,10 +24,11 @@ class InsertTransactionTest < Minitest::Test
   def test_insert_transaction
     FilePath.stub :users, @test_users do
     FilePath.stub :prices, FilePath.test_prices do
-      before_balance = get_balance
+      repo = Repo.new
+      before_balance = get_balance(repo)
       delta = -1234
       insert_transaction(delta)
-      after_balance = get_balance
+      after_balance = get_balance(repo)
       expected_balance = {}
       after_balance.each do |user_slug, balance|
         expected_balance[user_slug] = before_balance[user_slug] + delta
--- rb/test/repo.rb
+++ rb/test/repo.rb
@@ -11,55 +11,51 @@ class RepoTest < Minitest::Test
     @test_users = 'rb/test/tmp/users.json'
     @test_prices = 'rb/test/tmp/prices.json'
     @market_price = 25
-    @repo = Repo.new
     FileUtils.cp(FilePath.test_users, @test_users)
     FileUtils.cp(FilePath.test_prices, @test_prices)
   end
 
-  def insert_statuses(user_slug, statuses)
-    FilePath.stub :users, @test_users do
-    FilePath.stub :prices, @test_prices do
-      Market.stub :get_price, @market_price do
-        return @repo.create_statuses!(user_slug, statuses)
-      end
-    end
+  def _insert_statuses(repo, user_slug, statuses)
+    Market.stub :get_price, @market_price do
+      return repo.create_statuses!(user_slug, statuses)
     end
   end
 
-  def assert_status_inserted(card_name, maindeck, sideboard)
+  def _assert_status_inserted(card_name, maindeck, sideboard)
     user_slug = 'mpw'
     statuses = [{
       'name' => card_name,
       'maindeck' => maindeck,
       'sideboard' => sideboard,
     }]
-    res = insert_statuses(user_slug, statuses)
     expected = {
       :name => card_name,
       :price => @market_price,
       :maindeck => maindeck,
       :sideboard => sideboard,
     }
-    assert_equal res[:cards][card_name], expected
     FilePath.stub :users, @test_users do
     FilePath.stub :prices, @test_prices do
-      res = @repo.load_user_cards(user_slug)
+      repo = Repo.new
+      res = _insert_statuses(repo, user_slug, statuses)
+      assert_equal res[:cards][card_name], expected
+      res = repo.load_user_cards(user_slug)
+      assert_equal res[:cards][card_name], expected
     end
     end
-    assert_equal res[:cards][card_name], expected
   end
 
   def test_insert_maindeck
-    assert_status_inserted 'Fog', 0, 1
+    _assert_status_inserted 'Fog', 0, 1
   end
 
   def test_insert_maindeck_unusual
-    assert_status_inserted 'Aether Burst', 0, 1
+    _assert_status_inserted 'Aether Burst', 0, 1
   end
 
   def test_insert_maindeck_fake
     assert_raises(IllegalCardException) {
-      assert_status_inserted 'fart', 0, 1
+      _assert_status_inserted 'fart', 0, 1
     }
   end
 end
